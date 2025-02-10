929a9c0045bfad155030f6964105ceccd508f887
Syed Suleman <sydsulmn7@gmail.com>
2025-02-10 11:28:17 +0530

    added api impl with createAsyncThunk

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -1,12 +1,14 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReducer = require("../features/cake/cakeSlice");
+const iceCreamReducer = require("../features/icecream/iceCreamSlice");
+const userReducer = require("../features/user/userSlice");
 const reduxLogger = require("redux-logger");
 const logger = reduxLogger.createLogger();
-const iceCreamReducer = require("../features/icecream/iceCreamSlice");
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: iceCreamReducer,
+    user:userReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -2,19 +2,20 @@ const store = require('./app/store');
 console.log(store);
 const { cakeActions } = require("./features/cake/cakeSlice");
 const { iceCreamSliceActions } = require('./features/icecream/iceCreamSlice');
+const { fetchUsers } = require('./features/user/userSlice');
 console.log("initial State", store.getState());
 const unsubscribe = store.subscribe(() => {
   console.log("updated state", store.getState());
 });
+store.dispatch(fetchUsers())
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
 
-
-store.dispatch(iceCreamSliceActions.ordered());
-store.dispatch(iceCreamSliceActions.ordered());
-store.dispatch(iceCreamSliceActions.ordered());
-store.dispatch(iceCreamSliceActions.restocked(3));
-unsubscribe();
+// store.dispatch(iceCreamSliceActions.ordered());
+// store.dispatch(iceCreamSliceActions.ordered());
+// store.dispatch(iceCreamSliceActions.ordered());
+// store.dispatch(iceCreamSliceActions.restocked(3));
+// unsubscribe();
